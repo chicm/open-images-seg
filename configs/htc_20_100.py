2bb5cc239c531569b52319b856ec3afb327dd18f
chicm <idv2005@gmail.com>
2019-09-10 03:11:30 +0000

    updates

--- configs/htc_20_100.py
+++ configs/htc_20_100.py
@@ -262,7 +262,7 @@ data = dict(
         test_mode=True))
 # optimizer
 #optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
-optimizer = dict(type='Adam', lr=0.0001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='Adam', lr=0.0001, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(

cd68d2ac064800e971a320b67d433c4e8fd8eb79
chicm <idv2005@gmail.com>
2019-09-17 15:50:34 +0000

    updates

--- seg/ensemble.py
+++ seg/ensemble.py
@@ -16,8 +16,14 @@ from multiprocessing import Pool
 from settings import DATA_DIR, TEST_IMG_DIR
 from utils import get_image_size, parallel_apply, encode_binary_mask, general_ensemble
 
-preds1, preds2, classes, ens_dets = None, None, None, []
-preds3 = None
+pred_files = [
+    '../work_dirs/htc_level1_275/preds_0916pm_all_lb4456.pkl',
+    '../preds_0902_3_50_all_lb04195.pkl',
+    '../work_dirs/htc_level1_275/preds_0913pm_all_lb4343.pkl'
+]
+ens_weights = [0.5, 0.2, 0.3]
+
+all_preds, ens_dets, classes = [], [], None
 
 MAX_NUM = 70
 
@@ -33,34 +39,26 @@ def get_fn(img_id):
 
 def get_dets(preds, idx):
     box_pred, mask_pred = preds[idx]
-    #masks, labels, confs = [], [], []
     dets = []
     for i in range(len(classes)):
         for encoded_mask, bbox in zip(mask_pred[i], box_pred[i]):
             if True:
                 mask = mutils.decode(encoded_mask)
-                #masks.append(mask)
-                #labels.append(classes[i])
-                #confs.append(bbox[4])
+                # bbox[4] is confidence
                 dets.append([mask, classes[i], bbox[4]])
                 
     return dets
 
 def get_ens_det(idx):
-    det1 = get_dets(preds1, idx)
-    det2 = get_dets(preds2, idx)
-    det3 = get_dets(preds3, idx)
+    dets = [get_dets(p, idx) for p in all_preds]
     
-    ens_det = general_ensemble([det1, det2, det3], weights=[0.5, 0.3, 0.2])
+    ens_det = general_ensemble(dets, weights=ens_weights)
     ens_det = [[encode_binary_mask(x[0].astype(np.bool)), x[1], x[2]] for x in ens_det]
 
-    #del det1, det2
     return sorted(ens_det, key=lambda x: x[2], reverse=True)[:MAX_NUM]
 
 def get_pred_str(idx):
-    #masks, labels, confs = get_mask(idx)
     res = []
-    #for mask, label, conf in zip(masks, labels, confs):
     for det in ens_dets[idx]:
         res.append(det[1])
         res.append('{:.7f}'.format(det[2]))
@@ -73,31 +71,23 @@ def set_pred_str(df):
     df['PredictionString'] = df.img_index.map(lambda x: get_pred_str(x))
     return df
 
-def submit(args):
-    global preds1, preds2, preds3, classes, ens_dets, MAX_NUM
+def ensemble(args):
+    global all_preds, classes, ens_dets, MAX_NUM
     MAX_NUM = args.max_num
 
     classes, _ = get_top_classes(args.start_index, args.end_index, args.class_file)
-    #print('loading {}...'.format(args.pred_file))
-    #with open(args.pred_file, 'rb') as f:
-    #    preds = pickle.load(f)
-    print('loading1...')
-    with open('../work_dirs/htc_level1_275/preds_0916pm_all_lb4456.pkl', 'rb') as f:
-        preds1 = pickle.load(f)
-    print('loading2...')
-    with open('../preds_0902_3_50_all_lb04195.pkl', 'rb') as f:
-        preds2 = pickle.load(f)
-    print('loading3...')
-    with open('../work_dirs/htc_level1_275/preds_0913pm_all_lb4343.pkl', 'rb') as f:
-        preds3 = pickle.load(f)
+    for fn in pred_files:
+        print('loading {} ...'.format(fn))
+        with open(fn, 'rb') as f:
+            all_preds.append(pickle.load(f))
 
-    print('len(preds):', len(preds1))
-    print('num classes of preds:', len(preds1[0][1]))
+    print('len(preds):', len(all_preds[0]))
+    print('num classes of preds:', len(all_preds[0][0][1]))
     print('specified num classes:', len(classes))
     #assert len(preds[0][1]) == len(classes)
 
     with Pool(24) as p:
-        num_imgs = len(preds1)
+        num_imgs = len(all_preds[0])
         #ens_dets = list(tqdm(iterable=p.map(get_ens_det, list(range(num_imgs))), total=num_imgs))
         ens_dets = p.map(get_ens_det, range(num_imgs))
     #num_imgs = len(preds1)
@@ -132,4 +122,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
 
-    submit(args)
+    ensemble(args)
